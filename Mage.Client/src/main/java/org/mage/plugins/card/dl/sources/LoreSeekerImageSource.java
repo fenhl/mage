0736ca11f211623bc4adf74479be16bbe824c503
Fenhl <fenhl@fenhl.net>
2019-10-30 20:38:12 +0000

    Add Lore Seeker card image source

---
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/LoreSeekerImageSource.java
@@ -0,0 +1,69 @@
+package org.mage.plugins.card.dl.sources;
+
+import org.mage.plugins.card.dl.DownloadServiceInfo;
+import org.mage.plugins.card.images.CardDownloadData;
+
+import java.util.*;
+
+/**
+ * @author Fenhl
+ */
+public enum LoreSeekerImageSource implements CardImageSource {
+    instance;
+
+    private static final Set<String> supportedSets = new LinkedHashSet<String>() {
+        {
+            add("AYR");
+            add("DMS");
+            add("ANK");
+            add("LDO");
+            add("TSL");
+            add("VLN");
+            add("JAN");
+            add("CC18");
+            add("HLW");
+            add("VST");
+            add("DHM");
+            add("RAK");
+            add("NET");
+            add("EAU");
+            add("SOU");
+        }
+    };
+
+    @Override
+    public CardImageUrls generateCardUrl(CardDownloadData card) throws Exception {
+        return new CardImageUrls("https://lore-seeker.cards/cards/" + card.getSet().toLowerCase(Locale.ENGLISH) + "/" + card.getCollectorId() + ".png");
+    }
+
+    @Override
+    public CardImageUrls generateTokenUrl(CardDownloadData card) throws Exception { return null; }
+
+    @Override
+    public boolean prepareDownloadList(DownloadServiceInfo downloadServiceInfo, List<CardDownloadData> downloadList) { return true; }
+
+    @Override
+    public String getNextHttpImageUrl() { return null; }
+
+    @Override
+    public String getFileForHttpImage(String httpImageUrl) { return null; }
+
+    @Override
+    public String getSourceName() { return "Lore Seeker"; }
+
+    @Override
+    public float getAverageSize() { return 267.34f; }
+
+    @Override
+    public int getTotalImages() { return -1; }
+
+    @Override
+    public void doPause(String httpImageUrl) {}
+
+    @Override
+    public ArrayList<String> getSupportedSets() {
+        ArrayList<String> supportedSetsCopy = new ArrayList<>();
+        supportedSetsCopy.addAll(supportedSets);
+        return supportedSetsCopy;
+    }
+}
